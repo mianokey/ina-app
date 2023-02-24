3480ba08af78db612b4cc704192cd6abbdf6ccf9
mianokey <mianokey@gmail.com>
2023-02-24 13:37:47 +0300

    edite db config details to remote

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('inaapp_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('nusetaco_inapp', 'nusetaco_admin', 'NUSETAADMIN2022..', {
+    host: "pld109.truehost.cloud ",
     dialect: "mysql"
 });
  
